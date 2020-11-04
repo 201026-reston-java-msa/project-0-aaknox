62e9629e104272970b358d8bf403a14049e5b02b
aaknox <azhya.knox@gmail.com>
2020-11-03 23:27:15 -0600

    fixed small Display class bug on main menu

--- src/main/java/com/revature/Display.java
+++ src/main/java/com/revature/Display.java
@@ -17,15 +17,15 @@ public class Display {
 		System.out.println("------------------------------------------------------------------------------------\n\n");
 
 		System.out.println("Options: ");
-		System.out.println("1 - open an account");
-		System.out.println("2 - make a deposit");
-		System.out.println("3 - check your balance");
-		System.out.println("4 - make a withdrawal");
-		System.out.println("5 - transfer funds to another account");
+		System.out.println("1 - Open an account");
+		System.out.println("2 - Make a deposit");
+		System.out.println("3 - Check your balance");
+		System.out.println("4 - Make a withdrawal");
+		System.out.println("5 - Transfer funds to another account");
 		System.out.println();
-		System.out.println("6 - Show customer account information");
-		System.out.println("7 - Show customer personal information");
-		System.out.println("8 - Check pending account applications");
+		System.out.println("6 - View all accounts");
+		System.out.println("7 - View all users");
+		System.out.println("8 - View pending account applications");
 		System.out.println();
 		System.out.println("9 - Approve/Deny applications for accounts");
 		System.out.println("10 - Cancel an account");
